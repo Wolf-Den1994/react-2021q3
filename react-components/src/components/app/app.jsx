919147a94c1e790e31b5478dafe7ba58938d563e
Denis <wolf-den@tut.by>
2021-07-30 11:56:59 +0300

    feat: add search-bar component

--- react-components/src/components/app/app.jsx
+++ react-components/src/components/app/app.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../header/header';
+import './app.scss';
 
 const App = () => (
   <>
-    <div>Hello world</div>
+    <Header />
   </>
 );
 
---
+++ react-components/src/components/header/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SearchBar from '../search-bar/search-bar';
+import './header.scss';
+
+const Header = () => (
+  <>
+    <header className="header">
+      <SearchBar />
+    </header>
+  </>
+);
+
+export default Header;
