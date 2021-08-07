4083736dacd2e390b61111a1627318c9481d9d15
Denis <wolf-den@tut.by>
2021-08-07 18:36:12 +0300

    feat: not found request and time create news

--- react-api/src/components/app/app.jsx
+++ react-api/src/components/app/app.jsx
@@ -16,6 +16,7 @@ const App = () => {
   const [page, setPage] = useState(1);
   const [totalResults, setTotalResults] = useState();
   const [loading, setLoading] = useState(false);
+  const [empty, setEmpty] = useState(false);
 
   useEffect(() => {
     newsServise
@@ -24,28 +25,49 @@ const App = () => {
         setDate(response);
         setTotalResults(response.totalResults);
         if (Object.keys(response).length) setLoading(false);
+        if (Object.prototype.hasOwnProperty.call(response, 'articles')) {
+          if (Object.keys(response.articles).length) {
+            setEmpty(true);
+          } else {
+            setEmpty(false);
+          }
+        }
       })
       .catch();
-  }, [searchString, sort, numberResult, page, loading]);
+  }, [searchString, sort, numberResult, page, loading, empty]);
 
   const main = loading ? <Spinner /> : <Card data={data} />;
+
+  const emptySort = empty ? (
+    <Sort sortBy={sort} onSortBy={setSort} />
+  ) : (
+    <p className="empty">
+      No news for your request was found. Try another query.
+    </p>
+  );
+
+  const emptyPagination = empty ? (
+    <Pagination
+      num={numberResult}
+      changeNum={setNumberResult}
+      page={page}
+      changePage={setPage}
+      total={totalResults}
+    />
+  ) : (
+    <></>
+  );
 
   return (
     <>
       <SearchBar onSearch={setSearchString} onLoading={setLoading} />
-      {!Object.keys(data).length ? null : (
-        <Sort sortBy={sort} onSortBy={setSort} />
+      {!Object.keys(data).length && !empty ? <></> : emptySort}
+      {!Object.keys(data).length && !Object.keys(data).length && !loading ? (
+        <></>
+      ) : (
+        main
       )}
-      {!Object.keys(data).length && !loading ? <></> : main}
-      {!Object.keys(data).length ? null : (
-        <Pagination
-          num={numberResult}
-          changeNum={setNumberResult}
-          page={page}
-          changePage={setPage}
-          total={totalResults}
-        />
-      )}
+      {!Object.keys(data).length && !empty ? <></> : emptyPagination}
     </>
   );
 };
--- react-api/src/components/card/card.jsx
+++ react-api/src/components/card/card.jsx
@@ -4,13 +4,16 @@ import './card.scss';
 const Card = ({ data }) => {
   const { articles } = data;
   const elements = articles.map((elem) => {
+    const time = elem.publishedAt.split('T').join(' ').split(':');
     return (
       <div className="card" key={elem.url}>
         <img className="image" src={elem.urlToImage} alt={elem.title} />
         <div className="title">{elem.title}</div>
         <div className="description">{elem.description}</div>
         <div className="author">{elem.author}</div>
-        <div className="published">{elem.published}</div>
+        <div className="published">
+          {time[0]}:{time[1]}
+        </div>
         <div className="source">
           <span>
             <b>Source: </b>
