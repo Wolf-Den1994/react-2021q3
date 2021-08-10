a12180e5b3d5087b4fee68ae1926578a4200a77e
Denis <wolf-den@tut.by>
2021-08-10 18:39:24 +0300

    feat: add CSSTrantision and rederect notFonud

--- react-router/src/components/app/app.jsx
+++ react-router/src/components/app/app.jsx
@@ -1,60 +1,70 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import { CSSTransition } from 'react-transition-group';
 import Home from '../home/home';
 import Header from '../header/header';
 import About from '../about/about';
-import NotFound from '../not-found/not-found';
 import Details from '../details/details';
+import NotFound from '../not-found/not-found';
+import './app.scss';
 
 const App = () => {
   const [sort, setSort] = useState('popularity');
   const [searchString, setSearchString] = useState('');
 
-  const [showHome, setShowHome] = useState(true);
-  const [showAbout, setShowAbout] = useState(false);
-
-  console.log(showHome, showAbout)
+  const routes = [
+    { path: '/', Component: Home, id: 1 },
+    { path: '/about', Component: About, id: 2 },
+  ];
 
   return (
     <Router>
-      <Header onShowAbout={setShowAbout} />
+      <Header />
       <div className="app-wrapper">
-        <Switch>
-          <Route exact path="/">
-            {showHome && (
-              <Home
-                sort={sort}
-                setSort={setSort}
-                searchString={searchString}
-                setSearchString={setSearchString}
-              />
+        {routes.map(({ path, Component, id }) => (
+          <Route key={id} exact path={path}>
+            {({ match }) => (
+              <CSSTransition
+                timeout={300}
+                classNames="page"
+                unmountOnExit
+                in={match != null}
+              >
+                <Component
+                  sort={sort}
+                  setSort={setSort}
+                  searchString={searchString}
+                  setSearchString={setSearchString}
+                />
+              </CSSTransition>
             )}
           </Route>
-          <Route path="/about">
-            <CSSTransition
-              in={showAbout}
-              timeout={300}
-              classNames="fade"
-              unmountOnExit
-              onEnter={() => setShowHome(false)}
-              onExited={() => setShowHome(true)}
-            >
-              <About />
-            </CSSTransition>
-          </Route>
-          <Route
-            path="/details/:q/:id"
-            render={({ match }) => {
-              const id = match.params;
-              const q = match.params;
-              return <Details match={(id, q)} sort={sort} />;
-            }}
+        ))}
+        {/* <Route exact path="/">
+          <Home
+            sort={sort}
+            setSort={setSort}
+            searchString={searchString}
+            setSearchString={setSearchString}
           />
-          <Route>
-            <NotFound />
-          </Route>
-        </Switch>
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route> */}
+        <Route
+          path="/details/:q/:id"
+          render={({ match }) => {
+            const id = match.params;
+            const q = match.params;
+            return <Details match={(id, q)} sort={sort} />;
+          }}
+        />
+        <Route exact path="/404" component={NotFound} />
+        {window.location.pathname !== '/' &&
+        window.location.pathname !== '/about' &&
+        window.location.pathname.split('/')[1] !== 'details' ? (
+          <Redirect to="/404" />
+        ) : null}
       </div>
     </Router>
   );
--- react-router/src/components/header/header.jsx
+++ react-router/src/components/header/header.jsx
@@ -2,28 +2,18 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './header.scss';
 
-const Header = ({ onShowAbout }) => {
+const Header = () => {
   return (
     <header className="header">
       <nav>
         <ul>
           <li>
-            <NavLink
-              exact
-              activeClassName="active-link"
-              to="/"
-              onClick={() => onShowAbout(false)}
-            >
+            <NavLink exact activeClassName="active-link" to="/">
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink
-              exact
-              activeClassName="active-link"
-              to="/about"
-              onClick={() => onShowAbout(true)}
-            >
+            <NavLink exact activeClassName="active-link" to="/about">
               About
             </NavLink>
           </li>
