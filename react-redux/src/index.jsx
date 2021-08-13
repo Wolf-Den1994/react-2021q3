2f942974fe689b8b0bfc485c8927730794325142
Denis <wolf-den@tut.by>
2021-08-13 12:02:17 +0300

    feat: add redux and create first store

--- react-redux/src/index.jsx
+++ react-redux/src/index.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store/store';
 import App from './components/App/App';
 import './styles.scss';
+import { Counter } from './features/counter/Counter';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+    <Counter />
+  </Provider>,
+  document.getElementById('root'),
+);
